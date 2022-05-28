ae04041bee90d0cdfe61a7a2f13f9bd925147ce9
Wangsuifan <wsf_work@163.com>
2022-05-28 14:55:47 +0800

    Initial commit

---
+++ src/api/userMG.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+// 全局路径 请求的路径
+// axios.defaults.baseURL = 'http://ip:8.130.21.96'
+import { loginreq, req } from './axiosFun';
+
+// 登录接口
+export const login = (params) => { return loginreq("post", "/api/user/login", params) };
+// 获取验证码
+// export const getcodema = () =>{
+//   return axios.get("/api/kaptcha.jpg",{responseType:'blob'}).then(res =>res.data)
+// }
+// 获取用户菜单
+export const menu = (params) => { return axios.get("/api/menu?&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+// 退出接口
+export const loginout = () => { return req("post", "/api/user/logout") };
+
+/**
+ * 用户管理
+ **/
+// 用户管理-获取用户列表
+export const userList = (params) => { return req("post", "/api/user/list", params) };
+// 用户管理-保存（添加编辑）
+export const userSave = (params) => { return req("post", "/api/user/save", params) };
+// 用户管理-保存(核对用户名)
+export const checkUserName = (params) => {return req("post", "/api/user/checkUserNme", params)}
+// 用户管理-删除用户
+export const userDelete = (params) => { return req("post","/api/user/delete",params)};
+// export const userDelete = (params) => { return axios.delete("/api/deleteById?id=" + params + "&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+// 用户管理-重置密码
+export const userPwd = (params) => { return req("post", "/api/user/resetpwd", params) };
+// 用户管理-修改状态
+export const userStatus = (params) => { return req("post", "/api/user/status", params) };
+// 用户管理-分配角色
+export const changeRole = (params) => { return  req("post", "/api/user/changeRole", params) };
+// 用户管理-数据权限保存
+export const UserDeptSave = (params) => { return req("post", "/api/UserDept/save", params) };
+// 用户管理-获取部门设置
+export const UserDeptdeptTree = (params) => { return axios.get("/api/UserDept/deptTree/" + params + "?token=" + localStorage.getItem('logintoken')) };
+// 用户管理-保存部门设置
+export const UserChangeDept = (params) => { return req("post", "/api/User/change/dept", params) };
+// 用户管理-用户下线
+export const userExpireToken = (params) => { return req("get", "/api/User/expireToken/" + params, {}) };
+// 用户管理-刷新缓存
+export const userFlashCache = (params) => { return req("get", "/api/User/flashCache/" + params, {}) };
+
+/**
+ * 菜单管理
+ **/
+// 菜单管理-获取菜单Module/list
+export const ModuleList = () => { return req("post", "/api/Module/list") };
+// 菜单管理-根据菜单获取数据
+export const ModuleGet = (params) => { return axios.get("/api/Module/get/" + params + "?token=" + localStorage.getItem('logintoken')) };
+// 菜单管理-获取父级菜单Module/nodes
+export const ModuleNodes = (params) => { return req("post", "/api/Module/nodes", params) };
+// 菜单管理-修改菜单
+export const ModuleSave = (params) => { return req("post", "/api/Module/save", params) };
+// 菜单管理-删除菜单
+export const ModuleDelete = (params) => { return axios.delete("/api/Module/delete?ids=" + params + "&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+
+/**
+ * 角色管理
+ **/
+// 角色管理-获取角色列表
+export const roleList = (params) => { return req("post", "/api/role/list", params) };
+// 角色管理-保存（添加编辑）
+export const roleSave = (params) => { return req("post", "/api/role/save", params) };
+// 角色管理-删除角色
+export const roleDelete = (params) => { return req("post","/api/role/delete",params) };
+// 角色管理-菜单权限（获取）
+export const RoleRightTree = (params) => { return  req("post", "/api/role/menu", params)};
+// 角色管理-菜单权限（获取树）
+export const MenuTree = (params) => { return req("post","/api/menu/tree",params) };
+
+// 角色管理-菜单权限（保存）
+export const RoleRightSave = (params) => { return req("post", "/api/role/changemenu", params) };
+
+/**
+ * 公司管理
+ **/
+// 公司管理-获取公司列表
+export const deptList = (params) => { return req("post", "/api/Dept/list", params) };
+// 公司管理-保存（添加编辑）
+export const deptSave = (params) => { return req("post", "/api/Dept/save", params) };
+// 公司管理-删除公司
+export const deptDelete = (params) => { return axios.get("/api/Dept/delete?ids=" + params + "&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+
+/**
+ * 系统环境变量
+ **/
+// 系统环境变量-获取系统环境变量列表
+export const variableList = (params) => { return req("post", "/api/Variable/list", params) };
+// 系统环境变量-保存（添加编辑）
+export const variableSave = (params) => { return req("post", "/api/Variable/save", params) };
+// 系统环境变量-删除系统环境变量
+export const variableDelete = (params) => { return axios.delete("/api/Variable/delete?ids=" + params + "&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+
+/**
+ * 权限管理
+ **/
+// 权限管理-获取权限列表
+export const permissionList = (params) => { return req("post", "/api/menu/list", params) };
+// 权限管理-修改状态
+export const menuStatus = (params) => { return req("post", "/api/menu/status", params) };
+// 权限管理-保存权限
+export const ermissionSave = (params) => { return req("post", "/api/menu/save", params) };
+// 权限管理-删除权限
+export const ermissionDelete = (params) => {return req("post","/api/menu/delete",params) };
+// 权限管理-获取权限
+export const roleDropDown = () => { return axios.get("/api/Role/dropDown/all?&token=" + localStorage.getItem('logintoken')).then(res => res.data) };
+// 权限管理-配置权限
+export const RolePermission = (params) => { return req("post", "/api/RolePermission/save", params) };
---
+++ src/views/system/Permission.vue
@@ -0,0 +1,399 @@
+/**
+ * 系统管理  权限管理
+ */
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索筛选 -->
+    <el-form :inline="true" :model="formInline" class="user-search">
+      <el-form-item label="搜索：">
+        <el-input size="small" v-model="formInline.menuName" placeholder="输入权限名称"></el-input>
+      </el-form-item>
+      <el-form-item label="">
+        <el-input size="small" v-model="formInline.perms" placeholder="输入权限CODE"></el-input>
+      </el-form-item>
+<!--      <el-form-item label="角色：">-->
+<!--        <el-select size="small" v-model="formInline.roleId" placeholder="请选择">-->
+<!--          <el-option selected label="请选择" value="0"></el-option>-->
+<!--          <el-option v-for="parm in userparm" :key="parm.roleId" :label="parm.roleName" :value="parm.roleId"></el-option>-->
+<!--        </el-select>-->
+<!--      </el-form-item>-->
+      <el-form-item>
+        <el-button size="small" type="primary" icon="el-icon-search" @click="search">搜索</el-button>
+        <el-button size="small" type="primary" icon="el-icon-plus" @click="handleEdit()">添加</el-button>
+<!--        <el-button size="small" type="primary" icon="el-icon-plus" @click="RolePermission()">配置权限</el-button>-->
+      </el-form-item>
+    </el-form>
+    <!--列表-->
+    <el-table size="small" @selection-change="selectChange" :data="listData" highlight-current-row v-loading="loading" border element-loading-text="拼命加载中" style="width: 100%;">
+      <el-table-column align="center" type="selection" width="60">
+      </el-table-column>
+      <el-table-column sortable prop="id" label="编号" width="120">
+      </el-table-column>
+      <el-table-column sortable prop="menuName" label="权限名称" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="perms" label="权限CODE" width="300">
+      </el-table-column>
+      <el-table-column sortable prop="createTime" label="创建时间" width="300">
+        <template slot-scope="scope">
+          <div>{{scope.row.createTime|timestampToTime}}</div>
+        </template>
+      </el-table-column>
+<!--      <el-table-column sortable prop="editUser" label="修改人" width="300">-->
+<!--      </el-table-column>-->
+      <el-table-column align="center" sortable prop="status" label="状态" min-width="50">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.status==='0'?nshow:fshow" active-color="#13ce66" inactive-color="#ff4949"
+                     @change="editType(scope.$index, scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="操作" min-width="300">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="deleteUser(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <Pagination v-bind:child-msg="pageparm" @callFather="callFather"></Pagination>
+    <!-- 编辑界面 -->
+    <el-dialog :title="title" :visible.sync="editFormVisible" width="30%" @click="closeDialog">
+      <el-form label-width="120px" :model="editForm" :rules="rules" ref="editForm">
+        <el-form-item label="权限编号">
+          <el-input size="small" v-model="editForm.id" auto-complete="off" placeholder="权限编号" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="权限名称" prop="menuName">
+          <el-input size="small" v-model="editForm.menuName" auto-complete="off" placeholder="权限名称"></el-input>
+        </el-form-item>
+        <el-form-item label="权限CODE" prop="perms">
+          <el-input size="small" v-model="editForm.perms" auto-complete="off" placeholder="权限CODE"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button size="small" @click="closeDialog">取消</el-button>
+        <el-button size="small" type="primary" :loading="loading" class="title" @click="submitForm('editForm')">保存</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import {
+    permissionList,
+    ermissionSave,
+    ermissionDelete,
+    roleDropDown,
+    RolePermission,
+    menuStatus
+  } from '../../api/userMG'
+import Pagination from '../../components/Pagination'
+export default {
+  data() {
+    return {
+      nshow: true, //switch开启
+      fshow: false, //switch关闭
+      loading: false, //是显示加载
+      editFormVisible: false, //控制编辑页面显示与隐藏
+      title: '添加',
+      editForm: {
+        id: '',
+        menuName: '',
+        perms: '',
+        // token: localStorage.getItem('logintoken')
+      },
+      // rules表单验证
+      rules: {
+        menuName: [
+          { required: true, message: '请输入权限名称', trigger: 'blur' }
+        ],
+        perms: [
+          { required: true, message: '请输入权限CODE', trigger: 'blur' }
+        ]
+      },
+      formInline: {
+        page: 1,
+        limit: 10,
+        menuName: '',
+        perms: '',
+        roleId: '0',
+        // token: localStorage.getItem('logintoken')
+      },
+      // 选择数据
+      selectdata: [],
+      userparm: [], //搜索权限
+      listData: [], //用户数据
+      // 分页参数
+      pageparm: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 10
+      }
+    }
+  },
+  // 注册组件
+  components: {
+    Pagination
+  },
+  /**
+   * 数据发生改变
+   */
+
+  watch: {},
+
+  /**
+   * 创建完毕
+   */
+
+  created() {
+    this.getdata(this.formInline)
+    this.getAccsee()
+  },
+
+  /**
+   * 里面的方法只有被调用才会执行
+   */
+
+  methods: {
+    // 获取数据方法
+    getdata(parameter) {
+      this.loading = true
+
+      /***
+       * 调用接口，注释上面模拟数据 取消下面注释
+       */
+      // 获取权限列表
+      permissionList(parameter)
+        .then(res => {
+          this.loading = false
+          if (res.status === 500) {
+            this.$message({
+              type: 'warning',
+              message: res.msg
+            })
+          } else {
+            this.listData = res.data.records
+            // 分页赋值
+            this.pageparm.currentPage = this.formInline.page
+            this.pageparm.pageSize = this.formInline.limit
+            this.pageparm.total = res.data.total
+          }
+        })
+        .catch(err => {
+          console.log(err)
+          this.loading = false
+          this.$message.error('权限管理列表获取失败，请稍后再试！')
+        })
+    },
+    // 获取权限
+    getAccsee() {
+      roleDropDown()
+        .then(res => {
+          if (res.success == false) {
+            this.$message({
+              type: 'info',
+              message: res.msg
+            })
+          } else {
+            this.userparm = res.data
+          }
+        })
+        .catch(err => {
+          this.$message.error('权限获取失败，请稍后再试！')
+        })
+    },
+    // 修改type
+    editType: function (index, row) {
+      this.loading = true
+      let parm = {
+        id: '',
+        status: '',
+      }
+      parm.id = row.id
+      let status = row.status
+      if (status === '0') {
+        parm.status = '1'
+      } else {
+        parm.status = '0'
+      }
+      // 修改状态
+      menuStatus(parm).then(res => {
+        this.loading = false
+        if (res.status === 500) {
+          this.$message({
+            type: 'error',
+            message: res.msg
+          })
+        } else {
+          this.$message({
+            type: 'success',
+            message: res.msg
+          })
+          this.getdata(this.formInline)
+        }
+      })
+    },
+    // 分页插件事件
+    callFather(parm) {
+      this.formInline.page = parm.currentPage
+      this.formInline.limit = parm.pageSize
+      this.getdata(this.formInline)
+    },
+    // 搜索事件
+    search() {
+      this.getdata(this.formInline)
+    },
+    //显示编辑界面
+    handleEdit: function(index, row) {
+      this.editFormVisible = true
+      // 深拷贝并赋值
+      //this.editForm = Object.assign({}, row)
+      // console.log(row.id)
+      if (row !== undefined && row !== 'undefined') {
+        this.title = '修改'
+        this.editForm.id = row.id
+        this.editForm.menuName = row.menuName
+        this.editForm.perms = row.perms
+      } else {
+        this.title = '添加'
+        this.editForm.id = ''
+        this.editForm.menuName = ''
+        this.editForm.perms = ''
+      }
+    },
+    // 编辑、增加页面保存方法
+    submitForm(editData) {
+      this.$refs[editData].validate(valid => {
+        if (valid) {
+          ermissionSave(this.editForm)
+            .then(res => {
+              this.editFormVisible = false
+              this.loading = false
+              if (res.status === 200) {
+                this.getdata(this.formInline)
+                this.$message({
+                  type: 'success',
+                  message: res.msg
+                })
+              } else {
+                this.$message({
+                  type: 'info',
+                  message: res.msg
+                })
+              }
+            })
+            .catch(err => {
+              this.editFormVisible = false
+              this.loading = false
+              this.$message.error('权限管理保存失败，请稍后再试！')
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    // 删除权限
+    deleteUser(index, row) {
+      this.$confirm('确定要删除吗?', '信息', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          let parm = {
+            id: row.id,
+          }
+          ermissionDelete(parm)
+            .then(res => {
+              if (res.status ===200) {
+                this.$message({
+                  type: 'success',
+                  message: res.msg
+                })
+                this.getdata(this.formInline)
+              } else {
+                this.$message({
+                  type: 'info',
+                  message: res.msg
+                })
+              }
+            })
+            .catch(err => {
+              this.loading = false
+              this.$message.error('权限管理删除失败,请稍后再试！')
+            })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    // 选择复选框事件
+    selectChange(val) {
+      this.selectdata = val
+    },
+    // 配置权限
+    RolePermission() {
+      let parms = {
+        roleId: '',
+        permissionIds: ''
+      }
+      if (this.formInline.roleId === '0') {
+        this.$message({
+          type: 'info',
+          message: '请选择角色'
+        })
+        return false
+      }
+      parms.roleId = this.formInline.roleId
+      let len = this.selectdata
+      let ids = []
+      if (len != 0) {
+        for (let i = 0; i < len.length; i++) {
+          ids.push(len[i].permissionId)
+        }
+      }
+      parms.permissionIds = ids.join(',')
+      RolePermission(parms)
+        .then(res => {
+          if (res.success) {
+            this.$message({
+              type: 'success',
+              message: '配置权限成功！'
+            })
+            this.getdata(this.formInline)
+          } else {
+            this.$message({
+              type: 'info',
+              message: res.msg
+            })
+          }
+        })
+        .catch(err => {
+          this.loading = false
+          this.$message.error('配置权限失败,请稍后再试！')
+        })
+    },
+    // 关闭编辑、增加弹出框
+    closeDialog() {
+      this.editFormVisible = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-search {
+  margin-top: 20px;
+}
+.userRole {
+  width: 100%;
+}
+</style>
+
